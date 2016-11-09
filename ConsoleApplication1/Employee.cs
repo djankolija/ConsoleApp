d926baca15db8388c035df359de94d11ba06d6b2
Davor <Davor@DESKTOP-61FBALU>
2016-11-09 16:03:29 +0100

    Add project files.

---
+++ ConsoleApplication1/Employee.cs
@@ -0,0 +1,7 @@
+﻿class Employee : Person
+{
+    public override void SayFullName()
+    {
+        System.Console.WriteLine($"I am employee: {FirstName} {LastName}");
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name:");
+
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Your full name is: {firstName} {lastName}");
+
+            Console.ReadKey();
+        }
+    }
+}
